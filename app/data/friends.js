dbd42216adaec52ba9716a91c46100d3b3c3a778
mike4747 <mike4747@oh.rr.com>
2019-04-20 18:54:23 -0400

    validation is working on input fields, changed a few questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,26 +1,26 @@
 "use strict";
 
 var friendsArray = [
-    {name: "Olivia", photo_url: "Olivia_url", scores: [2, 4, 2, 4, 2, 4, 3, 3, 4, 1]},
-    {name: "Amelia", photo_url: "Amelia_url", scores: [4, 2, 4, 2, 2, 4, 3, 4, 4, 5]},
-    {name: "Isla", photo_url: "Isla_url", scores: [5, 4, 2, 5, 2, 3, 2, 3, 5, 3]},
-    {name: "Emily", photo_url: "Emily_url", scores: [5, 3, 2, 2, 1, 4, 2, 2, 5, 4]},
-    {name: "Ava", photo_url: "Ava_url", scores: [1, 3, 2, 4, 2, 3, 2, 4, 4, 4]},
-    {name: "Lily", photo_url: "Lily_url", scores: [3, 2, 4, 2, 4, 4, 5, 5, 1, 4]},
-    {name: "Mia", photo_url: "Mia_url", scores: [3, 2, 3, 4, 4, 2, 4, 4, 5, 3]},
-    {name: "Sophia", photo_url: "Sophia_url", scores: [2, 2, 2, 3, 3, 4, 4, 1, 4, 2]},
-    {name: "Isabella", photo_url: "Isabella_url", scores: [2, 2, 4, 3, 1, 3, 2, 2, 1, 4]},
-    {name: "Grace", photo_url: "Grace_url", scores: [2, 4, 4, 1, 2, 4, 3, 1, 5, 2]},
-    {name: "Oliver", photo_url: "Oliver_url", scores: [4, 2, 3, 4, 3, 2, 1, 4, 1, 2]},
-    {name: "Harry", photo_url: "Harry_url", scores: [2, 5, 4, 5, 2, 1, 2, 1, 2, 3]},
-    {name: "Jack", photo_url: "Jack_url", scores: [1, 1, 1, 5, 2, 4, 1, 4, 4, 3]},
-    {name: "George", photo_url: "George_url", scores: [5, 4, 1, 4, 2, 4, 4, 3, 1, 5]},
-    {name: "Noah", photo_url: "Noah_url", scores: [1, 4, 4, 2, 4, 5, 4, 3, 4, 4]},
-    {name: "Charlie", photo_url: "Charlie_url", scores: [5, 2, 2, 3, 3, 4, 4, 1, 5, 5]},
-    {name: "Jacob", photo_url: "Jacob_url", scores: [1, 1, 3, 4, 3, 1, 1, 5, 3, 1]},
-    {name: "Alfie", photo_url: "Alfie_url", scores: [3, 3, 4, 4, 3, 4, 4, 2, 3, 4]},
-    {name: "Freddie", photo_url: "Freddie_url", scores: [2, 4, 2, 4, 3, 3, 4, 5, 2, 1]},
-    {name: "Oscar", photo_url: "Oscar_url", scores: [4, 4, 1, 4, 4, 2, 4, 1, 5, 2]}];
+    {name: "Olivia", photo_url: "https://thewondrous.com/wp-content/uploads/2015/05/drunk-funny-people.jpg", scores: [2, 4, 2, 4, 2, 4, 3, 3, 4, 1]},
+    {name: "Amelia", photo_url: "https://britishexpats.com/forum/attachments/lounge-7/93731d1276690729-ugly-people-world-uglypeople-12.jpg", scores: [4, 2, 4, 2, 2, 4, 3, 4, 4, 5]},
+    {name: "Isla", photo_url: "http://www.freakingnews.com/pictures/128000/Rodney-Dangerfield-as-an-Old-Woman--128364.jpg", scores: [5, 4, 2, 5, 2, 3, 2, 3, 5, 3]},
+    {name: "Maude", photo_url: "http://1.bp.blogspot.com/-GUfNKddTPjw/TiHqKtQsSZI/AAAAAAAABNs/dI20Fns-U5E/s1600/Funny+old+woman+1.jpg", scores: [5, 3, 2, 2, 1, 4, 2, 2, 5, 4]},
+    {name: "Ava", photo_url: "http://www.teamjimmyjoe.com/wp-content/uploads/2013/11/Onion.jpg", scores: [1, 3, 2, 4, 2, 3, 2, 4, 4, 4]},
+    {name: "Lily", photo_url: "http://cf.ltkcdn.net/seniors/images/std/204242-675x450-funelderlylady.jpg", scores: [3, 2, 4, 2, 4, 4, 5, 5, 1, 4]},
+    {name: "Mia", photo_url: "http://www.teamjimmyjoe.com/wp-content/uploads/2012/11/Funny-Mug-Shots-ugly-woman.jpg", scores: [3, 2, 3, 4, 4, 2, 4, 4, 5, 3]},
+    {name: "Sophia", photo_url: "https://www.askideas.com/media/10/Old-Woman-Smoking-And-Making-Funny-Face-People.jpg", scores: [2, 2, 2, 3, 3, 4, 4, 1, 4, 2]},
+    {name: "Isabella", photo_url: "https://i.pinimg.com/originals/86/29/39/86293972a918a15aa6ddef51c9249001.jpg", scores: [2, 2, 4, 3, 1, 3, 2, 2, 1, 4]},
+    {name: "Grace", photo_url: "https://www.askideas.com/media/36/African-Girl-With-Weird-Piercing-Face-Funny-Picture.jpg", scores: [2, 4, 4, 1, 2, 4, 3, 1, 5, 2]},
+    {name: "Oliver", photo_url: "https://thewondrous.com/wp-content/uploads/2015/05/funny-pictures-of-ugly-people.jpg", scores: [4, 2, 3, 4, 3, 2, 1, 4, 1, 2]},
+    {name: "Bong", photo_url: "https://i.pinimg.com/originals/d0/a3/70/d0a370d019ba39d10ea6d64aabc63530.jpg", scores: [2, 5, 4, 5, 2, 1, 2, 1, 2, 3]},
+    {name: "Stubby", photo_url: "https://i.ytimg.com/vi/iQTZCj0sm-U/hqdefault.jpg", scores: [1, 1, 1, 5, 2, 4, 1, 4, 4, 3]},
+    {name: "George", photo_url: "https://www.biography.com/.image/t_share/MTE5NDg0MDU1MTc0MDIyNjcx/darryl-f.jpg", scores: [5, 4, 1, 4, 2, 4, 4, 3, 1, 5]},
+    {name: "Noah", photo_url: "https://www.topbestpics.com/wp-content/uploads/2018/01/wtf-ugly-people.jpg", scores: [1, 4, 4, 2, 4, 5, 4, 3, 4, 4]},
+    {name: "Charlie", photo_url: "http://4.bp.blogspot.com/_LK3Jc8YZXjs/TAOGaNLCy8I/AAAAAAAAWXo/XgC1xubhLq0/s1600/funny-gun-people-07.jpg", scores: [5, 2, 2, 3, 3, 4, 4, 1, 5, 5]},
+    {name: "Jacob", photo_url: "https://cdn.acidcow.com/pics/20100714/people_of_the_day_34.jpg", scores: [1, 1, 3, 4, 3, 1, 1, 5, 3, 1]},
+    {name: "Alfie", photo_url: "http://www.topbestpics.com/wp-content/uploads/2017/11/ugly-people-pics-28.jpg", scores: [3, 3, 4, 4, 3, 4, 4, 2, 3, 4]},
+    {name: "Freddie", photo_url: "http://www.nytimes.com/images/blogs/freakonomics/posts/Ugsalesman.jpg", scores: [2, 4, 2, 4, 3, 3, 4, 5, 2, 1]},
+    {name: "Oscar", photo_url: "https://www.bestfunnies.com/wp-content/uploads/2015/06/Ugly-People-Pics-4.jpg", scores: [4, 4, 1, 4, 4, 2, 4, 1, 5, 2]}];
 
 module.exports = friendsArray;
 
